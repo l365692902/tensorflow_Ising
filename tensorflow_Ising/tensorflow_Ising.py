5e051150b33e1d260c3e8bb01d83c7fc64292757
Honghao Yue <l365692902@gmail.com>
2017-02-07 23:33:27 +0800

    image and label ready

--- tensorflow_Ising/tensorflow_Ising.py
+++ tensorflow_Ising/tensorflow_Ising.py
@@ -1,4 +1,37 @@
 import tensorflow as tf
 import numpy as np
 
-print('test')
+def next_batch(images, labels, batch_size = 100):
+    """this function imitate the "next_batch" in \Python35\Lib\site-packages\tensorflow\contrib\learn\python\learn\datasets\mnist.py"""
+    """need a variable 'position', initialized with 0"""
+    global position
+    #print(position)
+    _start = position
+    _end = position + batch_size
+    _num_examples = labels.shape[0]
+    if batch_size > _num_examples:
+        print('[next_batch]batch_size oversized')
+        return
+    if _end > _num_examples:
+        rng_state = np.random.get_state()
+        np.random.shuffle(images)
+        np.random.set_state(rng_state)
+        np.random.shuffle(labels)
+        _start = 0
+        position = batch_size
+        _end = batch_size
+    position=_end
+    return images[_start:_end],labels[_start:_end]
+
+data = np.load('./Ising_N10_20000.npz')
+images = data['images']
+labels = data['labels']
+
+#images=np.array([[1,0],[2,0],[3,0],[4,0],[5,0],[6,0]])
+#labels=np.array([[1,0],[2,0],[3,0],[4,0],[5,0],[6,0]])
+position=0
+print(next_batch(images,labels,6))
+print(next_batch(images,labels,6))
+
+    
+    
